060e6a6cfbd784a2a20161fb73978c1e52e549ec
pongpong <jef15938@gmail.com>
2020-04-27 18:18:57 +0800

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ export const routes: Routes = [
 @NgModule({
   imports: [
     RouterModule.forRoot(routes),
-    RouterModule.forRoot(routes, { useHash: true })
+    // RouterModule.forRoot(routes, { useHash: true })
   ],
   exports: [RouterModule]
 })
